4de811773207dd18caacea4b0043c50496f7f7d5
umesh <umeshmahajan67@gmail.com>
2021-01-18 21:32:04 +0530

    Initial commit

---
+++ Context/ProductsContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using Eshop.Models;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+
+namespace Eshop.Context
+{
+    public class ProductsContext: DbContext
+    {
+        public ProductsContext() : base("ProductsContextDB")
+        { }
+
+        public DbSet<Category> Category { get; set; }
+        public DbSet<Product>Product { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
